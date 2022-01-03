4dbb40ee0d5ac2cb1b815750e408b7583110d501
cagrikucukbesleme <cagrikucukbesleme@gmail.com>
2022-01-03 15:36:04 +0300

    initial commit

---
+++ src/main/java/com/libraryproject/libraryproject/LibraryProjectApplication.java
@@ -0,0 +1,14 @@
+package com.libraryproject.libraryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProjectApplication.class, args);
+	}
+
+
+}
